9431c3cd81cdf84976ec54b5a49830f5d2e9bc23
karthiktesyantra <karthik.k@testyantra.com>
2023-07-12 11:26:29 +0530

    commit by manohara

--- TestVagrantAssignment.js
+++ TestVagrantAssignment.js
@@ -17,6 +17,12 @@ class Playlist {
       console.log(`${i + 1}. ${this.songs[i]}`);
     }
   }
+
+  assert(condition, message) {
+    if (!condition) {
+      console.error(`Assertion failed: ${message}`);
+    }
+  }
 }
 
 const myPlaylist = new Playlist(3);
@@ -25,13 +31,20 @@ myPlaylist.addSong("S2");
 myPlaylist.addSong("S3");
 
 //When S4 song is played -> S2,S3,S4
+myPlaylist.assert(myPlaylist.songs.length === 3, "S4 is not added");
 myPlaylist.addSong("S4");
 myPlaylist.displaySongs();
+myPlaylist.assert(myPlaylist.songs.length === 3, "S4 is added");
+
 
 //When S2 song is played -> S3,S4,S2
+myPlaylist.assert(myPlaylist.songs.length === 3, "S2 is not added");
 myPlaylist.addSong("S2");
 myPlaylist.displaySongs();
+myPlaylist.assert(myPlaylist.songs.length === 3, "S2 is added");
 
 //When S1 song is played -> S4,S2,S1
+myPlaylist.assert(myPlaylist.songs.length === 3, "S1 is not added");
 myPlaylist.addSong("S1");
 myPlaylist.displaySongs();
+myPlaylist.assert(myPlaylist.songs.length === 3, "S1 is added");
